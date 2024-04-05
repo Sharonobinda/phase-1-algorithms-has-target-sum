b5cd71adde76933a3114eba280df3cfe2f6ca486
sharonobinda <obindasharon6@gmail.com>
2024-04-05 23:17:45 +0300

    target sum

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+  hasTargetSum([5, 7, 13, 4, 11, 6], 12);
+// returns true, since 5 and 7 add up to 12
+
+hasTargetSum([20, 18, 3, 5, 30], 25);
+// returns true, since 20 and 5 add up to 25
+
+hasTargetSum([3, 8, 7], 12);
+// returns false, since no pair of numbers adds up to 12
 }
 
 /* 
@@ -18,17 +26,18 @@ function hasTargetSum(array, target) {
 if (require.main === module) {
   // add your own custom tests in here
   console.log("Expecting: true");
-  console.log("=>", hasTargetSum([3, 8, 12, 4, 11, 7], 10));
+  console.log("=>", hasTargetSum([5, 7, 13, 4, 11, 6], 12));
 
   console.log("");
 
   console.log("Expecting: true");
-  console.log("=>", hasTargetSum([22, 19, 4, 6, 30], 25));
+  console.log("=>", hasTargetSum([20, 18, 3, 5, 30], 25));
 
   console.log("");
 
   console.log("Expecting: false");
-  console.log("=>", hasTargetSum([1, 2, 5], 4));
+
+  console.log("=>", hasTargetSum([3, 8, 7], 12));
 }
 
 module.exports = hasTargetSum;
